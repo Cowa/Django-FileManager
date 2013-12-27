f1e5c53d6bffa8fe1eb7c3b780d725a6ff1a801a
Brice Thomas <brice.p.thomas@gmail.com>
2013-12-27 13:39:36 +0100

    Folder & root view displaying subfolders and files

--- manager/urls.py
+++ manager/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, url
 from manager import views
 
 urlpatterns = patterns('',
+    # URL pattern : adress/, root
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<folder_id>\d+)/$', views.test, name='test')
+    # URL pattern : address/4, where 4 is a folder's id
+    url(r'^(?P<folder_id>\d+)/$', views.folder, name='folder')
 )
--- manager/views.py
+++ manager/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from manager.models import Folder
+from manager.models import Folder, File
 
 def index(request):
-	context = {'path': "/"}
-	return render(request, 'manager/folder.html', context)
+	# Get the root (must be unique or error)
+	fold = get_object_or_404(Folder, parent=None)
+	return folder(request, fold.id)
 
-def test(request, folder_id):
+def folder(request, folder_id):
 	folder = get_object_or_404(Folder, pk=folder_id)
-	context = {'path': folder}
+	subfolders = Folder.objects.all().filter(parent=folder_id)
+	files = File.objects.all().filter(folder=folder_id)
+	context = {'path': folder, 'subfolders': subfolders, 'files': files}
 	return render(request, 'manager/folder.html', context)
